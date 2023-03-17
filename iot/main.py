28c773f67bb22bc2b0e7a4a0a08ae07cd6f26782
hibecung123 <son.nguyen1612@hcmut.edu.vn>
2023-03-17 14:34:05 +0700

    fixing some gateway

--- iot/main.py
+++ iot/main.py
@@ -1,10 +1,12 @@
+import serial.tools.list_ports
+import random
 import time
 import  sys
 from  Adafruit_IO import  MQTTClient
-from  uart import *
-AIO_FEED_IDs =["nutnhan1,nutnhan2"]
+
+AIO_FEED_IDs = ["nutnhan1","nutnhan2"]
 AIO_USERNAME = "hibecung123"
-AIO_KEY = "aio_lbtq23oB0afgjs4jTJAa3FlY1cBV"
+AIO_KEY = "aio_SyKT197IcJzMdwkCTk92gx97rodx"
 
 def  connected(client):
     print("Ket noi thanh cong...")
@@ -12,17 +14,15 @@ def  connected(client):
         client.subscribe(topic)
 
 def  subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong...")
+    print("Subcribe thanh cong...")
 
 def  disconnected(client):
     print("Ngat ket noi...")
     sys.exit (1)
 
 def  message(client , feed_id , payload):
-    print("Nhan du lieu: " +"tu "+feed_id+" "+ payload)
-    if isMicrobitConnected:
-        ser.write((str(payload)+"#").encode())
-
+    print("Nhan du lieu: " + payload)
+    ser.write((str(payload) + "#").encode())
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -32,7 +32,46 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB-SERIAL CH340" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+ser = serial.Serial(getPort(),115200)
+
+mess = ""
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[0] == "T":
+        client.publish("cambiennhiet", splitData[1])
+    if splitData[0] == "L":
+        client.publish("cambienanhsang", splitData[1])
+
+mess = ""
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
 while True:
-    if isMicrobitConnected:
-        readSerial(client)
+    readSerial()
     time.sleep(1)
