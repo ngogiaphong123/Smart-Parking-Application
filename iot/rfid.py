253ee0a4b9b070baae003bf2da01bab0cbd71703
hibecung123 <son.nguyen1612@hcmut.edu.vn>
2023-04-09 11:39:00 +0700

    gateway for rfid

--- iot/rfid.py
+++ iot/rfid.py
@@ -22,7 +22,7 @@ def  disconnected(client):
 
 def  message(client , feed_id , payload):
     ser.write((feed_id+ ":" + str(payload) + "#").encode())
-    print("!"+feed_id+ ":" + str(payload) + "#")
+    print(feed_id+ ":" + str(payload) + "#")
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -32,59 +32,43 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
-def getPort(type):
+def getPort():
     ports = serial.tools.list_ports.comports()
     N = len(ports)
     commPort = "None"
     for i in range(0, N):
         port = ports[i]
         strPort = str(port)
-        if(type == 1):
-            if "USB-SERIAL CH340" in strPort:
-                splitPort = strPort.split(" ")
-                commPort = (splitPort[0])
-        elif(type== 2):
-            if "Arduino Uno" in strPort:
-                splitPort = strPort.split(" ")
-                commPort = (splitPort[0])
+        if "Arduino Uno" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
     return commPort
 
-ser = serial.Serial(getPort(1), 115200)
-rfid = serial.Serial(getPort(2), 9600)
+ser = serial.Serial(getPort(),9600)
+
 mess = ""
-rfidmess = ""
 def processData(data):
     data = data.replace("!", "")
     data = data.replace("#", "")
     splitData = data.split(":")
     print(splitData)
-    if splitData[0] == "T":
-        client.publish("cambiennhiet", splitData[1])
-    if splitData[0] == "P":
-        client.publish("cambienhongngoai", splitData[1])
-    if splitData[0] == "rfid":
+    if(splitData[0]=="rfid"):
         client.publish("thongbao", splitData[1])
-
-def serial_parser(message):
-    while ("#" in message) and ("!" in message):
-        start = message.find("!")
-        end = message.find("#")
-        processData(message[start:end + 1])
-        if (end == len(message)):
-            message = ""
-        else:
-            message = message[end+1:]
+mess = ""
 def readSerial():
     bytesToRead = ser.inWaiting()
-    rfidbytesToRead = rfid.inWaiting()
     if (bytesToRead > 0):
         global mess
         mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        serial_parser(mess)
-    if (rfidbytesToRead > 0):
-        global rfidmess
-        rfidmess = rfidmess + rfid.read(rfidbytesToRead).decode("UTF-8")
-        serial_parser(rfidmess)
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
 while True:
     readSerial()
     time.sleep(1)
