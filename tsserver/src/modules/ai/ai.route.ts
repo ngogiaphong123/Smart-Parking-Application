1483f66c5f35c9a1024cd8f97bc5df152cef826c
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-05-01 14:13:12 +0700

    test ai route

--- tsserver/src/modules/ai/ai.route.ts
+++ tsserver/src/modules/ai/ai.route.ts
@@ -3,5 +3,7 @@ import aiAnswerHandle from './ai.controller';
 const aiRouter = express.Router();
 
 aiRouter.post('/aiAnswer', aiAnswerHandle) 
-
-export default aiRouter;  
+aiRouter.get('/test', (req, res) => {
+    res.send('Hello World!');
+})
+export default aiRouter; 
