9d75056c560c85d54e356706f2d3337df1b165e8
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-05-02 11:33:38 +0700

    Barchart of logs per hour, per day, per week

--- tsserver/src/modules/statistic/statistic.route.ts
+++ tsserver/src/modules/statistic/statistic.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import catchAsync from "../../utils/catchAsync";
-import { numLogsInDayController, slotPieChartController } from "./statistic.controller";
+import { numLogsInDayController, numLogsInMonthController, numLogsInWeekController, slotPieChartController } from "./statistic.controller";
 import zodMiddlewares from "../../middlewares/zodValid";
-import { DaySchema } from "./statistic.schema";
+import { StartDateSchema } from "./statistic.schema";
+import { requireUser } from "../../middlewares/requireUser";
 
 const statisticRouter = Router();
 
 statisticRouter.get("/slotPercentage", catchAsync(slotPieChartController))
-statisticRouter.post("/logPerHour", zodMiddlewares(DaySchema, "body"), catchAsync(numLogsInDayController))
+statisticRouter.post("/logPerHour", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInDayController))
+statisticRouter.post("/logPerDay", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInWeekController))
+statisticRouter.post("/logPerWeek", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInMonthController))
 
+statisticRouter.post("/customer/logPerHour", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInDayController))
+statisticRouter.post("/customer/logPerDay", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInWeekController))
+statisticRouter.post("/customer/logPerWeek", zodMiddlewares(StartDateSchema, "body"), catchAsync(numLogsInMonthController))
 export default statisticRouter;
--- tsserver/src/modules/statistic/statistic.schema.ts
+++ tsserver/src/modules/statistic/statistic.schema.ts
@@ -1,20 +1,10 @@
 import { z } from "zod";
 
-export const DateSchema = z.object({
+export const StartDateSchema = z.object({
     start : z.string({
         // regex : 2023-04-26T11:50:00.126Z
         required_error : "To date is required"
     }).regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/),
-    end : z.string({
-        // regex
-        required_error : "From date is required"
-    }).regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/),
+    accountId : z.string({}).optional()
 })
-export const DaySchema = z.object({
-    day : z.string({
-        // regex : 2023-04-26
-        required_error : "Day is required"
-    }).regex(/^\d{4}-\d{2}-\d{2}$/),
-})
-export type DateInput = z.infer<typeof DateSchema>;
-export type DayInput = z.infer<typeof DaySchema>;
+export type StartDateInput = z.infer<typeof StartDateSchema>;
